27ff71535e1d43b43c7c3b7e9751c0622eb7b95e
Lyon Lu <lyonlu13@gmail.com>
2021-05-29 13:26:55 +0800

    [ex] first exercise

---
+++ database_test/index.js
@@ -0,0 +1,26 @@
+// firebase 設定參數
+var firebaseConfig = {
+  apiKey: "AIzaSyBi8ENY3mtEyY2Csyd8VAOecltsqv_ROFY",
+  authDomain: "dsc-ccu-website.firebaseapp.com",
+  projectId: "dsc-ccu-website",
+  storageBucket: "dsc-ccu-website.appspot.com",
+  messagingSenderId: "550816413589",
+  appId: "1:550816413589:web:8acb48cd88e0c6da0b289c",
+  measurementId: "G-6JEYMNP7TN",
+}
+
+//初始化
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
+
+//指定資料庫物件
+var db = firebase.firestore()
+
+//獲取特定集合的所有文件
+db.collection("test")
+  .get()
+  .then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data())
+    })
+  })
