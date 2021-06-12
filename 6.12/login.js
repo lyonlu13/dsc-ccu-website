a43a65eb3238707c335141abae870e1d41d5b96d
Lyon Lu <lyonlu13@gmail.com>
2021-06-12 16:01:48 +0800

    6/12 practice

---
+++ 6.12/login.js
@@ -0,0 +1,14 @@
+// firebase 設定參數
+var firebaseConfig = {
+  apiKey: "AIzaSyBi8ENY3mtEyY2Csyd8VAOecltsqv_ROFY",
+  authDomain: "dsc-ccu-website.firebaseapp.com",
+  projectId: "dsc-ccu-website",
+  storageBucket: "dsc-ccu-website.appspot.com",
+  messagingSenderId: "550816413589",
+  appId: "1:550816413589:web:8acb48cd88e0c6da0b289c",
+  measurementId: "G-6JEYMNP7TN",
+}
+
+//初始化
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
