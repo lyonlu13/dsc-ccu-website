5cd709c7e3ba2e18c8431a26a23662f8fc029c14
Jacquelin <euv0104@gmail.com>
2021-06-17 20:28:54 +0800

    practice

---
+++ Submit/6.12_郭憶萱/login.js
@@ -0,0 +1,29 @@
+// firebase 設定參數
+var firebaseConfig = {
+  apiKey: "AIzaSyBi8ENY3mtEyY2Csyd8VAOecltsqv_ROFY",
+  authDomain: "dsc-ccu-website.firebaseapp.com",
+  projectId: "dsc-ccu-website",
+  storageBucket: "dsc-ccu-website.appspot.com",
+  messagingSenderId: "550816413589",
+  appId: "1:550816413589:web:8acb48cd88e0c6da0b289c",
+  measurementId: "G-6JEYMNP7TN",
+}
+
+//初始化
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
+
+const auth = firebase.auth();
+
+function login(){
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+
+  firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+  .then(function(){
+    var user = auth.currentUser;
+    alert("Hello, " + user.email);
+  })
+  .catch(e => alert(e.message));
+  
+}
