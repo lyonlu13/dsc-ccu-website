2bdf537eb56aeb24db4441b1398a8925ff7df3d8
Jacquelin <euv0104@gmail.com>
2021-07-24 21:40:11 +0800

    announcement_from_firebase

--- deploy/index.js
+++ deploy/index.js
@@ -1,31 +1,57 @@
+//初始化
 var express = require('express');
 var router = express();
 var port = process.env.port || 8080;
-//寫了下面這一行就不能動
-//var engine = require('ejs-locals');
 
-
-//寫了下面這一行就不能動
-//router.engine('ejs', engine);
+//setting ejs
 router.set('views', './views');
 router.set('view engine', 'ejs');
 
 //setting CSS and picture.
 router.use(express.static(__dirname + '/public'))
 
+//setting firebase
+var admin = require("firebase-admin");
+var serviceAccount = require("./dsc-ccu-website-firebase-adminsdk-4rl7a-10b0593f24.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  //databaseURL: 'https://dsc-ccu-website.firebaseio.com'
+});
+
+//load data from firestore
+const db = admin.firestore()
+var rows = []
+
+db.collection('announcement').get()
+  .then(snapshot => {
+
+    snapshot.forEach(doc => {
+
+      const announceDetail = doc.data()
+      rows.push(announceDetail)
+    })
+    //convert timestamp to date
+    for (var i = 0; i < rows.length; i++) {
+      let thedate = rows[i].date.toDate();
+      let timestamp = new Date(thedate).getTime();
+      let month = new Date(timestamp).getMonth() + 1;
+      let year = new Date(timestamp).getFullYear();
+      let date = new Date(timestamp).getDate();
+      var original_date = year + '/' + month + '/' + date;
+      rows[i].date = original_date
+    }
+
+  })
+
+
+
 //index
-//在這裡更改字串資料
-router.get('/', function(req, res) {
-    res.render('index', {  //represent index.ejs
-    type: '分類'
-    ,time: '2021 07/14'
-    ,title: '標題'
-    ,description: `愛情走的太快 就像龍捲風
-    不能承受 我已無處可躲
-    我不要再想 我不要再想
-    我不 我不 我不要再想你`
-    
-    });  
+router.get('/', function (req, res) {
+  res.render('index', {
+    rows: rows,
+    description: 'hi everyone',
+    index: true
+  })
 })
 
 //匯入公告
@@ -37,6 +63,23 @@ var about = require('./routers/about.js');
 router.use('/about', about)
 
 //listen
-router.listen(port, function() {
-    console.log('已開啟 port: ' + port);
+router.listen(port, function () {
+  console.log('已開啟 port: ' + port);
 })
+
+
+
+// db.collection('announcement').get()
+//     .then(snapshot => {
+//       console.log(`found ${snapshot.size} documents.`)
+//       var rows = []
+//       snapshot.forEach((element) => {
+//         rows.push() = {
+//           titles: element.title
+//           ,types: element.type,
+//           dates: element.date
+//         }
+//       })
+//       res.render('index', {row: announce})
+//       console.log(rows)
+//     })
--- deploy/routers/announcement.js
+++ deploy/routers/announcement.js
@@ -1,13 +1,12 @@
 var express = require('express')
 var router = express.Router()
 
+
 //傳入資料(字串)在這裡改
 router.get('/', function(req, res) {
     res.render('announcement',{
-        type: '日記'
-        ,time: '2021 07/14'
-        ,title: '今天天氣真好'
-        ,isHomePage:false
+        index: false
+        
     })
 })
 
