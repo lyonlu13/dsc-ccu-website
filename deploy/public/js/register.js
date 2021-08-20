ec7b107185087bdb31abd9e77b017f64bbabe08e
Jacquelin <euv0104@gmail.com>
2021-08-20 20:50:22 +0800

    登入註冊完成

--- deploy/public/js/register.js
+++ deploy/public/js/register.js
@@ -1,45 +1,46 @@
 // firebase 設定參數
 var firebaseConfig = {
-    apiKey: "AIzaSyBi8ENY3mtEyY2Csyd8VAOecltsqv_ROFY",
-    authDomain: "dsc-ccu-website.firebaseapp.com",
-    projectId: "dsc-ccu-website",
-    storageBucket: "dsc-ccu-website.appspot.com",
-    messagingSenderId: "550816413589",
-    appId: "1:550816413589:web:8acb48cd88e0c6da0b289c",
-    measurementId: "G-6JEYMNP7TN",
-  }
-  
-  //初始化
-  firebase.initializeApp(firebaseConfig)
-  const auth = firebase.auth();
+  apiKey: "AIzaSyBi8ENY3mtEyY2Csyd8VAOecltsqv_ROFY",
+  authDomain: "dsc-ccu-website.firebaseapp.com",
+  projectId: "dsc-ccu-website",
+  storageBucket: "dsc-ccu-website.appspot.com",
+  messagingSenderId: "550816413589",
+  appId: "1:550816413589:web:8acb48cd88e0c6da0b289c",
+  measurementId: "G-6JEYMNP7TN",
+}
 
-  window.onload = function(){
+//初始化
+firebase.initializeApp(firebaseConfig)
+const auth = firebase.auth();
 
-    var registration_code = document.getElementById("registration_code");
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-    var check_password = document.getElementById("check_password");
+window.onload = function(){
 
-    document.getElementById('register-button').onclick = function(){
-      //check if password is the same
-      if(password.value!=check_password.value){
-        alert("你的密碼不相同")
-        return;
-      }
+  var registration_code = document.getElementById("registration_code");
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+  var check_password = document.getElementById("check_password");
 
-      //safe email and password to firebase
-      firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
-        .then((userCredential) => {
-          // Signed in 
-          //var user = userCredential.user;
-          location.assign('/management/report');
-
-        })
-        .catch((error) => {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          alert(errorMessage)
-        });
-      }
-
+  document.getElementById('register-button').onclick = function(){
+    //check if password is the same
+    if(password.value!=check_password.value){
+      alert("你的密碼不相同")
+      return;
     }
+
+    //safe email and password to firebase
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+      .then((userCredential) => {
+        // Signed in 
+        var uid = auth.currentUser.uid;
+        document.cookie = `uid=${uid};`
+        location.assign('/management/report');
+
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage)
+      });
+    }
+
+  }
