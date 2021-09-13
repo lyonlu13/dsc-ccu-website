ffcbebd566e3ab3808fc810ad3a8cbbbe1984703
Jacq <euv0104@gmail.com>
2021-09-13 13:52:42 +0800

    公告按日期排序

--- deploy/public/js/index.js
+++ deploy/public/js/index.js
@@ -18,31 +18,13 @@ var db = firebase.firestore();
 //當按下全部鍵
 window.onload = function(){
     document.getElementById('all').onclick = function(){
-      updateAnnouncement(0);
-    }
-
-    //當按下通知
-    document.getElementById('notification').onclick = function(){
-      updateAnnouncement(1);
-    }
-
-    //當按下活動
-    document.getElementById('event').onclick = function(){
-      updateAnnouncement(2);
-    }
-
-
-    function updateAnnouncement(type){
-      let condition = [["!=",""],["==","notification"],["==","event"]]
-      db.collection("announcement").where("type", condition[type][0], condition[type][1])
+      db.collection('announcement').orderBy('date')
       .get()
       .then((querySnapshot) => {
-        //清空列表資料
         let element = document.getElementById("items");
         while (element.firstChild) {
           element.removeChild(element.firstChild);
         }
-        //$( ".items" ).empty();
         querySnapshot.forEach((doc) => {
           console.log(doc.id, " => ", doc.data())
           //加入公告
@@ -73,12 +55,125 @@ window.onload = function(){
             </a>
           </ul>`; 
           document.getElementById("items").appendChild(announcement); 
-        });
+        })
       })
       .catch((error) => {
+        var errorMessage = error.message
+        console.log(errorMessage)
           //Todo(處理錯誤)
       });
-      }
+    }
+
+    //當按下通知
+    document.getElementById('notification').onclick = function(){
+      updateAnnouncement(0);
+    }
+
+    //當按下活動
+    document.getElementById('event').onclick = function(){
+      updateAnnouncement(1);
+    }
+    //create reference
+    var announceRef = db.collection('announcement')
+
+    function updateAnnouncement(type){
+      let condition = ['notification', 'event']
+      announceRef.where('type', '==', condition[type]).orderBy('date', 'asc')
+      .get()
+      .then((querySnapshot) => {
+        let element = document.getElementById("items");
+        while (element.firstChild) {
+          element.removeChild(element.firstChild);
+        }
+        querySnapshot.forEach((doc) => {
+          console.log(doc.id, " => ", doc.data())
+          //加入公告
+          var announcement = document.createElement("div");
+          announcement.classList.add("items");
+          let thedate = doc.data().date.toDate();
+          function getDate()
+          {
+              let dateObj = new Date(thedate);
+              let month = dateObj.getMonth()+1;
+              let year = dateObj.getFullYear();
+              let date = dateObj.getDate();
+              return `${year}/${month}/${date}`;
+          }
+          var typeString = ""
+          if(doc.data().type == 'event'){
+            typeString = '活動'
+          }
+          else{
+            typeString = '通知'
+          }
+          announcement.innerHTML =
+          `<ul id = "nodot" class="nodot">
+            <a href="" class="no-color-line">
+              <li class="type-slider type-slider-${doc.data().type}">${typeString}</li> 
+              <li class="middle">${getDate()}</li>
+              <li class="title">${doc.data().title}</li>
+            </a>
+          </ul>`; 
+          document.getElementById("items").appendChild(announcement); 
+        })
+      })
+      .catch((error) => {
+        var errorMessage = error.message
+        console.log(errorMessage)
+          //Todo(處理錯誤)
+      });
+    }
+
+
+    // function updateAnnouncement(type){
+    //   let condition = [["!=",""],["==","notification"],["==","event"]]
+    //   db.collection("announcement").where("type", condition[type][0], condition[type][1])
+    //   .get()
+    //   .then((querySnapshot) => {
+    //     //清空列表資料
+    //     let element = document.getElementById("items");
+    //     while (element.firstChild) {
+    //       element.removeChild(element.firstChild);
+    //     }
+    //     //$( ".items" ).empty();
+    //     querySnapshot.forEach((doc) => {
+    //       console.log(doc.id, " => ", doc.data())
+    //       //加入公告
+    //       var announcement = document.createElement("div");
+    //       announcement.classList.add("items");
+    //       let thedate = doc.data().date.toDate();
+    //       function getDate()
+    //       {
+    //           let dateObj = new Date(thedate);
+    //           let month = dateObj.getMonth()+1;
+    //           let year = dateObj.getFullYear();
+    //           let date = dateObj.getDate();
+    //           return `${year}/${month}/${date}`;
+    //       }
+    //       var typeString = ""
+    //       if(doc.data().type == 'event'){
+    //         typeString = '活動'
+    //       }
+    //       else{
+    //         typeString = '通知'
+    //       }
+    //       announcement.innerHTML =
+    //       `<ul id = "nodot" class="nodot">
+    //         <a href="" class="no-color-line">
+    //           <li class="type-slider type-slider-${doc.data().type}">${typeString}</li> 
+    //           <li class="middle">${getDate()}</li>
+    //           <li class="title">${doc.data().title}</li>
+    //         </a>
+    //       </ul>`; 
+    //       document.getElementById("items").appendChild(announcement); 
+    //     });
+    //   })
+    //   .catch((error) => {
+    //     var errorMessage = error.message
+    //     console.log(errorMessage)
+    //       //Todo(處理錯誤)
+    //   });
+    // }
     //----------------------------------------------------------聯繫我們-------------------------------------------------------------------------//
     var InsertName,  InsertEmail,  InsertMessage;
     var InsertStatus = "unread"
